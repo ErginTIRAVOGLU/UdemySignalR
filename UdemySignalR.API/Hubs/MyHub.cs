4009e2f0a8962e8f6a491ea4bee7ba7e799f6965
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2024-01-08 00:31:16 +0300

    Client count Added

--- UdemySignalR.API/Hubs/MyHub.cs
+++ UdemySignalR.API/Hubs/MyHub.cs
@@ -4,7 +4,10 @@ namespace UdemySignalR.API.Hubs;
 
 public class MyHub:Hub
 {
-    public static List<string> Names { get; set; } = new List<string>();
+    private static List<string> Names { get; set; } = new List<string>();
+    private static List<string> ClientList { get; set; } = new List<string>();
+    private static int ClientCount { get; set; } = 0;
+
     
     public async Task SendName(string name)
     {
@@ -14,6 +17,19 @@ public class MyHub:Hub
 
     public async Task GetNames()
     {
-        await Clients.All.SendAsync("ReciveNames", Names);
+        await Clients.All.SendAsync("ReceiveNames", Names);
+    }
+
+    public async override Task OnConnectedAsync()
+    {
+        ClientCount++;
+        await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+        await base.OnConnectedAsync();
+    }
+    public async override Task OnDisconnectedAsync(Exception? exception)
+    {
+        ClientCount--;
+        await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+        await base.OnDisconnectedAsync(exception);
     }
 }
